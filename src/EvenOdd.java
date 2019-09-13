eacfab67c83e34d56c3b12e16ea4eef53f0039e1
simran <macstudent@10.10.150.116>
2019-09-13 12:13:20 -0400

    initial commit

---
+++ src/EvenOdd.java
@@ -0,0 +1,9 @@
+//R1: Accepts 1 integer value, n
+//N > 1  
+//R2: If N < 1, then return false
+//R3: If n is even, return true
+//R4: If n is odd, return false
+
+public class EvenOdd {
+
+}
---
+++ tests/EvenOddTest.java
@@ -0,0 +1,30 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EvenOddTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	
+	
+//	R1: Accepts 1 integer value, n
+//	N > 1  
+//	R2: If N < 1, then return false
+//	R3: If n is even, return true
+//	R4: If n is odd, return false
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
